8569e2ee268d16441d8647e0a6cfeb0d529206ea
Amaken <benisante@gmail.com>
2020-03-31 11:35:12 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
-// Iteration 1: Names and Input
+console.log("I'm ready!");
+
+let hacker1 = "john"
+console.log("The driver's name is", hacker1)
+
+let hacker2 = "YYYY"
+console.log("The navigator's name is", hacker2)
+
+let driver = hacker1
+
+for(let i=1; i<hacker1.length; i++){
+   if(hacker1.length> hacker2.length){
+    console.log("The driver has the longest name it has , "+ hacker1.length +"characters.");
+    break
+  }
+  else if(hacker1.length> hacker2.length){
+    console.log("The driver has the longest name it has , "+ hacker1.length +"characters.");
+    break
+  }
+  else {
+    console.log("wow, you both have equally long names , "+ hacker1.length +" characters!.");
+    break
+  }
+}
+  console.log(driver.toUpperCase().split("").join(" "));
+  console.log(driver.split("").reverse().join(""));
 
 
-// Iteration 2: Conditionals
+if(hacker1.length<hacker2.length){console.log(hacker1 +"'s name goes first");}
 
+else if(hacker2.length<hacker1.length){console.log("Yo,"+hacker2+"goes first definitely");}
 
-// Iteration 3: Loops
+else {console.log("What?! You both have the same name?");}
